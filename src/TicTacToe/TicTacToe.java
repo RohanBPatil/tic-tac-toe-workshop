9dba937572ea990562505ea1343ee92ffe6ba6c1
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-08 08:58:07 +0530

    Added : new board

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,13 @@
+package TicTacToe;
+
+public class TicTacToe {
+	public char[] board;
+	
+	public TicTacToe() {
+		board = new char[10];
+	}
+	
+	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe();
+	}
+}

c40c8bcb82eb45e3dd7cbe20623939ec329c454f
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-08 09:42:03 +0530

    Added : select x or o for player and computer

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,13 +1,27 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
-	public char[] board;
+	public static String[] board;
 	
 	public TicTacToe() {
-		board = new char[10];
+		board = new String[10];
+		for(int i = 0; i < board.length; i++)
+			board[i] = "";
+	}
+	
+	public String chooseChar() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Select one x or o");
+		String playerChar = scanner.nextLine();
+		return playerChar;
 	}
 	
 	public static void main(String[] args) {
 		TicTacToe game = new TicTacToe();
+		String playerChar = game.chooseChar();
+		String compChar = (playerChar.equals("x"))? "o" : "x";
+		System.out.println(compChar);
 	}
 }
